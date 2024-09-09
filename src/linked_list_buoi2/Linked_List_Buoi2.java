e8bc0c7ad2e0842d197f817aa94d9844977773aa
MinhTri676 <tricmse182002@fpt.edu.vn>
2024-09-10 00:59:11 +0700

    Commit second

--- src/linked_list_buoi2/Linked_List_Buoi2.java
+++ src/linked_list_buoi2/Linked_List_Buoi2.java
@@ -7,9 +7,9 @@ public class Linked_List_Buoi2 {
 
     public static void main(String[] args) {
         MyLinkedList linkedList = new MyLinkedList();
-//        linkedList.addFirst(1);
-//        linkedList.addFirst(2);
-//        linkedList.addFirst(3);
+        linkedList.addFirst(1);
+        linkedList.addFirst(2);
+        linkedList.addFirst(3);
 
         linkedList.addLast(1);
         linkedList.addLast(2);
