31e33df170543244cb8e0aae4527235872701257
MinhTri676 <tricmse182002@fpt.edu.vn>
2024-09-10 00:37:38 +0700

    Commit second

---
+++ src/linked_list_buoi2/Linked_List_Buoi2.java
@@ -0,0 +1,19 @@
+
+package linked_list_buoi2;
+
+import mylinkedlist.MyLinkedList;
+
+public class Linked_List_Buoi2 {
+
+    public static void main(String[] args) {
+        MyLinkedList linkedList = new MyLinkedList();
+//        linkedList.addFirst(1);
+//        linkedList.addFirst(2);
+//        linkedList.addFirst(3);
+
+        linkedList.addLast(1);
+        linkedList.addLast(2);
+        linkedList.addLast(3);
+    }
+        
+}
---
+++ src/mylinkedlist/MyLinkedList.java
@@ -0,0 +1,36 @@
+
+package mylinkedlist;
+
+public class MyLinkedList {
+    private Node head;
+    
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node tmp = head;
+        while (tmp.next != null){
+            tmp = tmp.next;
+        }
+        // Sau câu lệnh này đã tìm được node cuối cùng và gán được cho tmp
+        tmp.next = newNode;
+    }
+
+    public void add(int data){
+        Node newNode = new Node(data);
+        ìf(index == 0){
+            addFirst(data);
+            return;
+        }else{
+            Node tmp = head;
+        }
+    }
+}
